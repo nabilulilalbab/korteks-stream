6df96b51f7bfc284ab69cdafb581d5398c711e7b
nabilulilalbab <nabilulilalbab78@gmail.com>
2025-07-28 20:55:47 +0700

    ui: dark better , light not recomended, ads

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ CSRF_TRUSTED_ORIGINS = [
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "*"]
 
